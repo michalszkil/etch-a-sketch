4db8c397ac0e2545e180f37aebdf8ab7b3554ee5
Michal Szkil <michalszkil@gmail.com>
2021-10-16 15:07:54 +0200

    Added basic sketching functionality

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function addSquare(main) {
+function addSquare(main, i) {
     square = document.createElement("div");
-    square.id = "square"
+    square.id = "square" + i;
+    square.className = "square";
     main.appendChild(square);
 }
 
 const main = document.getElementById("main");
 
 for (let i = 0; i < 16; i++) {
-    addSquare(main);
+    addSquare(main, i);
+}
+const squares = document.querySelectorAll('.square');
+
+for(let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover",function() {
+        // this.style.backgroundColor = "black";
+        this.classList.add("square-colored");
+    })
 }
