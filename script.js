8a9d2a9855ca5d9bb9847a5f7c2c5e4ec2484165
Michal Szkil <michalszkil@gmail.com>
2021-10-22 23:28:02 +0200

    Added a basic resizing functionality

--- script.js
+++ script.js
@@ -5,11 +5,25 @@ function addSquare(main, i) {
     main.appendChild(square);
 }
 
-const main = document.getElementById("main");
+function drawBoard(main, size) {
 
-for (let i = 0; i < 16; i++) {
-    addSquare(main, i);
+    for (let i = 0; i < size*size; i++) {
+        addSquare(main, i);
+    }
 }
+
+function clearBoard() {
+    document.getElementById("main").innerHTML = '';
+}
+
+let size = 40;
+
+
+let main = document.getElementById("main");
+
+drawBoard(main, size);
+
+
 const squares = document.querySelectorAll('.square');
 
 for(let i = 0; i < squares.length; i++) {
@@ -22,7 +36,51 @@ for(let i = 0; i < squares.length; i++) {
 button_clear = document.getElementById("btn-clear");
 
 button_clear.addEventListener("click", function() {
+    let squares = document.querySelectorAll('.square');
+
     for (let i = 0; i < squares.length; i++) {
-        squares[i].classList.remove("square-colored")
+        squares[i].classList.remove("square-colored");
     }
+})
+
+button_resize = document.getElementById("btn-resize");
+
+button_resize.addEventListener("click", function() {
+    size = document.getElementById('textbox').value
+    let squares1 = document.querySelectorAll('.square');
+
+    for (let i = 0; i < squares1.length; i++) {
+        squares1[i].classList.remove("square-colored");
+    }
+
+    let game = document.getElementById("game");
+
+    game.innerHTML = "";
+
+    let new_main = document.createElement("div");
+    new_main.id = "main";
+    // main1.style.width = 500;
+    // main1.style.height = 500;
+
+    
+    game.appendChild(new_main);
+    
+    document.getElementById("main").style.color ="green";
+    document.getElementById("main").style.height = String(size*10) + "px";
+    document.getElementById("main").style.width = String(size*10) + "px";
+
+
+    main.innerHTML = "";
+
+    drawBoard(new_main, size);
+
+    const squares = document.querySelectorAll('.square');
+
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("mouseover",function() {
+            // this.style.backgroundColor = "black";
+            this.classList.add("square-colored");
+        })
+    }
+
 })
